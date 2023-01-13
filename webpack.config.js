496a55d6367b58af8e49eb941a9b9696522f75ce
Klaus Eckelt <Klaus.Eckelt@jku.at>
2023-01-13 16:10:52 +0000

    cleanup debug configs

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,7 @@ module.exports = {
       {
         test: /\.js$/,
         enforce: 'pre',
-        use: [
-          {
-            loader: 'source-map-loader',
-            options: {
-              filterSourceMappingUrl: (url, resourcePath) => {
-                if (!resourcePath.includes('node_modules')) {
-                  console.log('filterSourceMappingUrl', url, resourcePath);
-                  return true; //'consume' the source map
-                }
-                return false;
-              }
-            }
-          }
-        ]
+        use: ['source-map-loader']
       }
     ]
   }
