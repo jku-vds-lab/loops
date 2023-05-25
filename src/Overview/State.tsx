ece5b0146f44fb22837ec3b306e2d0f434975470
Klaus Eckelt <Klaus.Eckelt@jku.at>
2023-05-25 13:06:12 +0000

    sidebar styling

--- src/Overview/State.tsx
+++ src/Overview/State.tsx
@@ -19,26 +19,42 @@ const useStyles = createStyles((theme, _params, getRef) => ({
     flexShrink: 0 //dont shrink, because then they will collapse as much as possible
   },
   state: {
-    // slight margin and border around state
-    // border: '1px solid #ccc',
-    // borderRadius: '0.5rem',
-
-    //make sure that the width and height of the parent container are note exceeded by the border
-    // boxSizing: 'border-box',
-
     height: '100%',
 
     // the state itself uses a flex layout to arrange its elements
-    // display: 'flex',
-    // flexDirection: 'column'
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.2rem 0',
 
-    // set .jp-InputArea-editor  to block to avoid overflowing the state column horizontally
-    "& .jp-InputArea-editor": {
-      display: 'block'
+    '& .jp-InputArea-editor': {
+      // set .jp-InputArea-editor  to block to avoid overflowing the state column horizontally
+      display: 'block',
+      //background: 'unset',
+      border: 'unset'
+    },
+
+    // give jp-cells a transparant border
+    '& .jp-Cell': {
+      border: '1px solid #bdbdbd',
+      padding: '0',
+      borderRadius: '0.5rem',
+
+      '&.active': {
+        borderLeft: '2px solid #1976d2 !important'
+      },
+
+      ' .input': {
+        // only set border radius on the top (as the input takes the upper half of the cell)
+        '& .jp-InputArea-editor': {
+          borderRadius: '0.5rem 0.5rem 0 0'
+        },
+
+        //if input is the only child, also set bottom radius
+        '&:only-child .jp-InputArea-editor': {
+          borderRadius: '0.5rem'
+        }
+      }
     }
-  },
-  active: {
-    'border-left': '4px solid #1976d2 !important'
   },
   unchanged: {
     color: 'transparent !important',
@@ -54,6 +70,9 @@ const useStyles = createStyles((theme, _params, getRef) => ({
     '& .html-diff-delete-inline-wrapper': {
       display: 'none'
     }
+  },
+  inOutSplit: {
+    borderTop: '1px solid #bdbdbd'
   }
 }));
 
@@ -64,7 +83,7 @@ interface IStateProps {
   stateNo: number;
 }
 
-export function State({ state, stateNo, previousState, fullWidth: current }: IStateProps): JSX.Element {
+export function State({ state, stateNo, previousState, fullWidth }: IStateProps): JSX.Element {
   const { classes, cx } = useStyles();
 
   if (!state) {
@@ -77,24 +96,42 @@ export function State({ state, stateNo, previousState, fullWidth: current }: ISt
 
     if (isMarkdown(cell.inputModel)) {
       // for markdown, show output (rendered markdown) instead of input (markdown source)
-      return (
-        <div>
-          {cell.outputHTML.map(output => (
-            <div className="input markdown" dangerouslySetInnerHTML={{ __html: (output as HTMLElement).outerHTML }} />
-          ))}
-        </div>
-      );
+      const markdownOutputs = cell.outputHTML.map(output => {
+        let content = output as HTMLElement;
+        if (!fullWidth) {
+          //remove all children that are not headers
+          content.querySelectorAll(':not(h1, h2, h3, h4, h5, h6)').forEach(child => child.remove());
+        }
+        return (
+          <div
+            className={cx('jp-Cell', { ['active']: isActiveCell === true })}
+            dangerouslySetInnerHTML={{ __html: content.outerHTML }}
+          />
+        );
+      });
+      return <div>{markdownOutputs}</div>;
     } else {
       // for code, show input (source code) and output (rendered output) next to each other
-      let input = getInput(cell, i, isActiveCell);
-      let output = getOutput(cell, i, isActiveCell);
+      let input = getInput(cell, i, isActiveCell, fullWidth);
+      let output = getOutput(cell, i, isActiveCell, fullWidth);
+
+      // check if output has content
+      const split =
+        output.props.children && output.props.children?.length > 0 ? (
+          <div className={cx(classes.inOutSplit)}></div>
+        ) : (
+          <></>
+        );
+
+      // create a cell with input and output
       return (
         <div
           className={cx('jp-Cell', {
-            [classes.active]: isActiveCell === true
+            ['active']: isActiveCell === true
           })}
         >
           {input}
+          {split}
           {output}
         </div>
       );
@@ -105,7 +142,7 @@ export function State({ state, stateNo, previousState, fullWidth: current }: ISt
   return (
     <div
       className={cx(classes.stateWrapper, 'stateWrapper', 'jp-Notebook', {
-        [classes.currentState]: current === true
+        [classes.currentState]: fullWidth === true
       })}
     >
       <div className={cx(classes.state, 'state')}>
@@ -117,44 +154,70 @@ export function State({ state, stateNo, previousState, fullWidth: current }: ISt
     </div>
   );
 
-  function getInput(cell: CellProvenance, i: number, isActiveCell: boolean) {
+  function getInput(cell: CellProvenance, i: number, isActiveCell: boolean, fullWidth: boolean) {
+    //Default: show the input as it is
     let input = (
-      <div
-        className="input jp-InputArea jp-Cell-inputArea"
-        dangerouslySetInnerHTML={{ __html: (cell.inputHTML as HTMLElement).outerHTML }} />
+      <div className="input">
+        <div
+          className="input jp-InputArea jp-Cell-inputArea jp-Editor jp-InputArea-editor"
+          dangerouslySetInnerHTML={{ __html: (cell.inputHTML as HTMLElement).outerHTML }}
+        />
+      </div>
     );
+
+    if (!fullWidth) {
+      //If the state is not full width, just show a single line as indicator
+      input = (
+        <div className="input">
+          <div className="jp-InputArea jp-Cell-inputArea jp-Editor jp-InputArea-editor">
+            <br />
+          </div>
+        </div>
+      );
+    }
+
+    //If there is a previous state, compare the input with the previous input
     if (previousState?.cells[i]?.inputHTML) {
       const diff = new HtmlDiff(
         (previousState.cells[i].inputHTML as HTMLElement).outerHTML,
         (cell.inputHTML as HTMLElement).outerHTML
       );
       const unifiedDiff = diff.getUnifiedContent();
+
       if (diff.newWords.length + diff.oldWords.length !== 0) {
+        //If there are changes, show the diff
         input = <div className="input" dangerouslySetInnerHTML={{ __html: unifiedDiff }} />;
       } else {
-        // is active?
-        if (isActiveCell) {
-          //Show the unchanged input in full height
+        // No changes to this cell
+        if (fullWidth && isActiveCell) {
+          // if the cell is active, show the input as it is
           input = (
             <div
               className={cx(classes.unchanged, 'input')}
-              dangerouslySetInnerHTML={{ __html: (cell.inputHTML as HTMLElement).outerHTML }} />
+              dangerouslySetInnerHTML={{ __html: (cell.inputHTML as HTMLElement).outerHTML }}
+            />
           );
         } else {
+          // if the cell is not active (and there are no changes), don't show the input at all
           // just indicate the code cell with a single line (== 1 <br>)
-          input = <div className={cx(classes.unchanged, 'input', 'jp-Editor', 'jp-InputArea-editor')}>
-            <br />
-          </div>;
+          input = (
+            <div className={cx(classes.unchanged, 'input')}>
+              <div className={cx('jp-Editor', 'jp-InputArea-editor')}>
+                <br />
+              </div>
+            </div>
+          );
         }
       }
     }
+
     return input;
   }
 
-  function getOutput(cell: CellProvenance, i: number, isActiveCell: boolean) {
+  function getOutput(cell: CellProvenance, i: number, isActiveCell: boolean, fullWidth: boolean) {
     let output = <></>;
 
-    if (isCode(cell.inputModel)) {
+    if (isCode(cell.inputModel) && cell.outputHTML.length > 0) {
       output = (
         <div className="outputs jp-OutputArea jp-Cell-outputArea">
           {cell.outputHTML.map((output, j) => {
@@ -175,13 +238,16 @@ export function State({ state, stateNo, previousState, fullWidth: current }: ISt
                   return (
                     <div
                       className={cx(classes.unchanged, 'output')}
-                      dangerouslySetInnerHTML={{ __html: (output as HTMLElement).outerHTML }} />
+                      dangerouslySetInnerHTML={{ __html: (output as HTMLElement).outerHTML }}
+                    />
                   );
                 } else {
                   // just indicate the output with a single line (== 1 <br>)
-                  return <div className={cx(classes.unchanged, 'output')}>
-                    <br />
-                  </div>;
+                  return (
+                    <div className={cx(classes.unchanged, 'output')}>
+                      <br />
+                    </div>
+                  );
                 }
               }
             }
