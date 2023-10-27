ac0e73c864064e69eee337964ccd32137004ff45
Klaus Eckelt <Klaus.Eckelt@jku.at>
2023-10-27 10:49:56 +0000

    ignore warings introduced with tabletojson package

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
+console.log('➡️  loading custom webpack config ');
+
 module.exports = {
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
   resolve: {
     fallback: {
       fs: false,
       // "path": false, // set in https://github.com/jupyterlab/jupyterlab/blob/v4.0.0/builder/src/webpack.config.base.ts#L66
       crypto: false
     }
-  }
+  },
+  ignoreWarnings: [
+    // some modules dont seem to have source maps, but I don't need them anyway
+    // exclude rule above seems to have no effect
+    /Failed to parse source map/,
+    // tabletojson version not found in the nested package.json. But it has a version in the top-level package.json
+    /No version specified and unable to automatically determine one/
+  ]
 };
