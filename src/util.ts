86fc50b3609fef8713560cffce8ff3a8b3e20e6f
Klaus Eckelt <Klaus.Eckelt@jku.at>
2024-01-08 09:41:00 +0100

    fix cell order for deleted cells (#80)

--- src/util.ts
+++ src/util.ts
@@ -18,16 +18,16 @@ export function mergeArrays(newArray: string[], oldArray: string[] | undefined):
     const newId = newArray[i];
     const oldId = oldArray[i];
 
-    // first add id from newArray (precedence order)
-    if (newId !== undefined) {
-      // could be out of bounds already when oldArray is longer
-      mergedArray.push(newId);
-    }
-
     // if the ids are different or the newArray is already over
     // and oldID is not in newArray, add it to the mergedArray
     if (oldId !== undefined && oldId !== newId && !newArray.includes(oldId)) {
       mergedArray.push(oldId);
+    }
+
+    // first add id from newArray (precedence order)
+    if (newId !== undefined) {
+      // could be out of bounds already when oldArray is longer
+      mergedArray.push(newId);
     }
   }
 
