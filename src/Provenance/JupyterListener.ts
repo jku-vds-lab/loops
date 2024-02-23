83b3d84fc4b083eec7e5800e99269ee2ed108704
Klaus Eckelt <Klaus.Eckelt@jku.at>
2024-02-23 11:57:14 +0000

    skip prov extraction for other notebooks

--- src/Provenance/JupyterListener.ts
+++ src/Provenance/JupyterListener.ts
@@ -10,7 +10,7 @@ export class JupyterListener {
   private serializer = new XMLSerializer();
   constructor(private nbtrrack: NotebookTrrack, private notebook: Notebook) {
     const trackCellChanges = this.trackCellChanges();
-    console.debug('JupyterListener trackCellChanges enabled?', trackCellChanges);
+    console.debug(notebook.title.label, 'JupyterListener trackCellChanges enabled?', trackCellChanges);
 
     // listener for changes of the active cell
     this.notebook.activeCellChanged.connect((notebook, args) => {
@@ -28,7 +28,7 @@ export class JupyterListener {
     // });
 
     const trackExecutions = this.trackExecutions();
-    console.debug('JupyterListener trackExecutions enabled?', trackExecutions);
+    console.debug(notebook.title.label, 'JupyterListener trackExecutions enabled?', trackExecutions);
 
     // fires likes 6 times when a cell is executed 😵‍💫
     // this.notebook.modelContentChanged.connect((notebookModel, args) => {
@@ -36,7 +36,7 @@ export class JupyterListener {
     // });
 
     this.notebook.modelChanged.connect((oldModel, newModel) => {
-      console.log('⚠️⚠️⚠️ JupyterListener modelChanged', oldModel, newModel);
+      console.log(notebook.title.label, '⚠️⚠️⚠️ JupyterListener modelChanged', oldModel, newModel);
     });
 
     if (notebook.activeCell) {
@@ -80,152 +80,153 @@ export class JupyterListener {
     args: { notebook: Notebook; cell: Cell<ICellModel>; success: boolean; error?: KernelError | null | undefined }
   ) {
     const { cell, notebook /*, success, error */ } = args; // not using success and error currently
-    console.log('JupyterListener cellExecuted', notebook);
-    notebook.model?.cells.get(0);
-
-    // console.log('notebook active cell id', notebook.activeCell?.id, notebook.activeCell?.model.id);
-    const prov: NotebookProvenance = {
-      cells: [],
-      // set active cell stored in notebook as fallback.
-      // Correc if "Execute and don't Advance" (Ctrl+Enter) was used
-      // when  "Execute and Advance" (Shift+Enter / Toolbar button) was used, the index is already updated to the next cell when this listener is executed
-      // therefore this index will be overwritten by getting the index of the executed cell (part of args) below
-      activeCellIndex: notebook.activeCellIndex,
-      activeCellID: notebook.activeCell?.id
-      // active cell also does not have to be the executed cell, e.g. when "Run All" or "Run All Above Selected Cell" is used
-    };
-    // console.log('JupyterListener cellExecuted - Active Cell', notebook.activeCellIndex);
-
-    const childs = toArray(notebook.children());
-    for (const [index, child] of childs.entries()) {
-      if (child instanceof Cell && child.inputArea) {
-        const input = child.inputArea;
-
-        // console.log(child.id, 'input', input);
-        // const dataset = child.dataset; // was empty thus far
-        //const children = toArray(child.children()); // all HTML elements that belong to a cell (header, footer, toolbar, input, output)
-        // --> input and output can be accessed separately
-        const inputModel = child.inputArea.model;
-
-        // get just the editor
-        const inputHTML = input.node.querySelector('.jp-InputArea-editor')?.cloneNode(true) as HTMLElement;
-        // remove all unneeded CodeMirror elements (e.g. line numbers, cursor, ...)
-        inputHTML.querySelectorAll('.CodeMirror>:not(.CodeMirror-scroll)')?.forEach(node => node.remove());
-        inputHTML.querySelectorAll('.cm-cursorLayer')?.forEach(node => node.remove());
-        inputHTML.querySelectorAll('.cm-selectionLayer')?.forEach(node => node.remove());
-
-        // remove cursor, selection, highlights, etc.
-        const hits = inputHTML.querySelectorAll(
-          '.jp-mod-completer-enabled, .jp-mod-focused, .cm-focused, .jp-mod-has-primary-selection'
-        );
-        hits?.forEach(node =>
-          node.classList.remove(
-            'jp-mod-completer-enabled',
-            'jp-mod-focused',
-            'cm-focused',
-            'jp-mod-has-primary-selection'
-          )
-        );
-
-        let cellProv: CellProvenance = {
-          id: child.inputArea.model.id, // same as child.model.id
-          type: inputModel.type,
-          inputModel: inputModel.toJSON(),
-          inputHTML: this.serializer.serializeToString(inputHTML),
-          outputHTML: Array.from(child.node.querySelectorAll('.jp-OutputArea-output')).map(node =>
-            this.serializer.serializeToString(node.cloneNode(true))
-          ),
-          active: cell === child
-        };
-        if (cellProv.active) {
-          prov.activeCellIndex = index;
-          prov.activeCellID = cellProv.id;
-          // console.log('JupyterListener cellExecuted - Executed Cell', index);
-        }
-
-        if (child instanceof CodeCell) {
-          //CodeCell extends cell
-          const outputArea = child.outputArea; //all outputs
-          // inidividual outputs are stored as children
-          // no output (e.g. imports) --> empty children array
-          // cells can have multiple outputs (e.g. print(), last cell line output, visualization)
-          //const outputs = toArray(outputArea.children());
-          // widgets actually gives the same data more easily:
-          // const widgets = outputArea.widgets;
-          // console.log('output widgets', widgets);
-          // console.log('code cell was executee #', child.model.executionCount);
-
-          // // widgets == representation, model == data
-          // for (let i = 0; i < outputArea.model.length; i++) {
-          //   const output: IOutputModel = outputArea.model.get(i);
-          //   const type = output.type as OutputType;
-          //   // types:
-          //   // * stream:  prints or streaming outputs (there can be multiple stream outputs, e.g., for stdout and stderr)
-          //   // * execute_result: last line of cell
-          //   // * display_data:  seaborn/matplotlib
-          //   // * update_display_data: update a display_data output
-          //   // * error: errors during code execution
-          //   // also see: https://jupyter-client.readthedocs.io/en/stable/messaging.html#execution-errors
-
-          //   const data = output.data;
-          //   // data:
-          //   // * text/plain: print() output
-          //   // * text/html: e.g., pandas dataframes
-          //   // * image/png: e.g., seaborn/matplotlib
-          //   // * image/jpeg
-          //   // * image/svg+xml
-          //   // * application/vnd.jupyter.stderr: print to stderr  (e.g. warnings)
-          //   // * application/vnd.jupyter.stdout: print to stdout
-
-          //   //executionCount is shown for input and output (but only for 'execute_result' type)
-          //   console.log('output', i, output.executionCount, type, Object.keys(output.data));
-          // }
-
-          // const model = notebook.model;
-          // const cell3 = model?.cells.get(3);
-          // console.log('cell type', cell3?.type, 'val', cell3?.sharedModel.getSource());
-          // console.log('cell', cell3);
-
-          cellProv = { ...cellProv, output: outputArea.model.toJSON() } as CodeCellProvenance;
-        } else if (child instanceof MarkdownCell) {
-          //MarkdownCell extends attachmentcell which extends cell
-          // console.log('markdown headlines', child.headingInfo);
-          //console.log('markdown headlines', child.headings); // requires jupyter 4
-
-          // output == rendered Markdown
-          // .jp-RenderedHTMLCommo
-          const outputHTML = Array.from(child.node.querySelectorAll('.jp-MarkdownOutput')).map(node =>
-            this.serializer.serializeToString(node.cloneNode(true))
-          );
-
-          // copy/pasted images, for example, are attachments
-          const attachments = child.model.attachments;
-          // console.log('attachments', attachments.keys);
-
-          // const attachmentData = attachments.get(attachments.keys[0]);
-          cellProv = { ...cellProv, attachments: attachments.toJSON(), outputHTML } as MarkdownCellProvenance;
-        } else if (child instanceof RawCell) {
-          //RawCell extends cell
-          // no special information
-          // console.log('raw');
-        } else {
-          // console.log('unknown cell');
-        }
-        prov.cells.push(cellProv);
-      }
-    }
-
     //check if it is the notebook tracked by this instance
     if (notebook.id === this.notebook.id) {
+      console.log('JupyterListener cellExecuted', notebook);
+      notebook.model?.cells.get(0);
+
+      // console.log('notebook active cell id', notebook.activeCell?.id, notebook.activeCell?.model.id);
+      const prov: NotebookProvenance = {
+        cells: [],
+        // set active cell stored in notebook as fallback.
+        // Correc if "Execute and don't Advance" (Ctrl+Enter) was used
+        // when  "Execute and Advance" (Shift+Enter / Toolbar button) was used, the index is already updated to the next cell when this listener is executed
+        // therefore this index will be overwritten by getting the index of the executed cell (part of args) below
+        activeCellIndex: notebook.activeCellIndex,
+        activeCellID: notebook.activeCell?.id
+        // active cell also does not have to be the executed cell, e.g. when "Run All" or "Run All Above Selected Cell" is used
+      };
+      // console.log('JupyterListener cellExecuted - Active Cell', notebook.activeCellIndex);
+
+      const childs = toArray(notebook.children());
+      for (const [index, child] of childs.entries()) {
+        if (child instanceof Cell && child.inputArea) {
+          const input = child.inputArea;
+
+          // console.log(child.id, 'input', input);
+          // const dataset = child.dataset; // was empty thus far
+          //const children = toArray(child.children()); // all HTML elements that belong to a cell (header, footer, toolbar, input, output)
+          // --> input and output can be accessed separately
+          const inputModel = child.inputArea.model;
+
+          // get just the editor
+          const inputHTML = input.node.querySelector('.jp-InputArea-editor')?.cloneNode(true) as HTMLElement;
+          // remove all unneeded CodeMirror elements (e.g. line numbers, cursor, ...)
+          inputHTML.querySelectorAll('.CodeMirror>:not(.CodeMirror-scroll)')?.forEach(node => node.remove());
+          inputHTML.querySelectorAll('.cm-cursorLayer')?.forEach(node => node.remove());
+          inputHTML.querySelectorAll('.cm-selectionLayer')?.forEach(node => node.remove());
+
+          // remove cursor, selection, highlights, etc.
+          const hits = inputHTML.querySelectorAll(
+            '.jp-mod-completer-enabled, .jp-mod-focused, .cm-focused, .jp-mod-has-primary-selection'
+          );
+          hits?.forEach(node =>
+            node.classList.remove(
+              'jp-mod-completer-enabled',
+              'jp-mod-focused',
+              'cm-focused',
+              'jp-mod-has-primary-selection'
+            )
+          );
+
+          let cellProv: CellProvenance = {
+            id: child.inputArea.model.id, // same as child.model.id
+            type: inputModel.type,
+            inputModel: inputModel.toJSON(),
+            inputHTML: this.serializer.serializeToString(inputHTML),
+            outputHTML: Array.from(child.node.querySelectorAll('.jp-OutputArea-output')).map(node =>
+              this.serializer.serializeToString(node.cloneNode(true))
+            ),
+            active: cell === child
+          };
+          if (cellProv.active) {
+            prov.activeCellIndex = index;
+            prov.activeCellID = cellProv.id;
+            // console.log('JupyterListener cellExecuted - Executed Cell', index);
+          }
+
+          if (child instanceof CodeCell) {
+            //CodeCell extends cell
+            const outputArea = child.outputArea; //all outputs
+            // inidividual outputs are stored as children
+            // no output (e.g. imports) --> empty children array
+            // cells can have multiple outputs (e.g. print(), last cell line output, visualization)
+            //const outputs = toArray(outputArea.children());
+            // widgets actually gives the same data more easily:
+            // const widgets = outputArea.widgets;
+            // console.log('output widgets', widgets);
+            // console.log('code cell was executee #', child.model.executionCount);
+
+            // // widgets == representation, model == data
+            // for (let i = 0; i < outputArea.model.length; i++) {
+            //   const output: IOutputModel = outputArea.model.get(i);
+            //   const type = output.type as OutputType;
+            //   // types:
+            //   // * stream:  prints or streaming outputs (there can be multiple stream outputs, e.g., for stdout and stderr)
+            //   // * execute_result: last line of cell
+            //   // * display_data:  seaborn/matplotlib
+            //   // * update_display_data: update a display_data output
+            //   // * error: errors during code execution
+            //   // also see: https://jupyter-client.readthedocs.io/en/stable/messaging.html#execution-errors
+
+            //   const data = output.data;
+            //   // data:
+            //   // * text/plain: print() output
+            //   // * text/html: e.g., pandas dataframes
+            //   // * image/png: e.g., seaborn/matplotlib
+            //   // * image/jpeg
+            //   // * image/svg+xml
+            //   // * application/vnd.jupyter.stderr: print to stderr  (e.g. warnings)
+            //   // * application/vnd.jupyter.stdout: print to stdout
+
+            //   //executionCount is shown for input and output (but only for 'execute_result' type)
+            //   console.log('output', i, output.executionCount, type, Object.keys(output.data));
+            // }
+
+            // const model = notebook.model;
+            // const cell3 = model?.cells.get(3);
+            // console.log('cell type', cell3?.type, 'val', cell3?.sharedModel.getSource());
+            // console.log('cell', cell3);
+
+            cellProv = { ...cellProv, output: outputArea.model.toJSON() } as CodeCellProvenance;
+          } else if (child instanceof MarkdownCell) {
+            //MarkdownCell extends attachmentcell which extends cell
+            // console.log('markdown headlines', child.headingInfo);
+            //console.log('markdown headlines', child.headings); // requires jupyter 4
+
+            // output == rendered Markdown
+            // .jp-RenderedHTMLCommo
+            const outputHTML = Array.from(child.node.querySelectorAll('.jp-MarkdownOutput')).map(node =>
+              this.serializer.serializeToString(node.cloneNode(true))
+            );
+
+            // copy/pasted images, for example, are attachments
+            const attachments = child.model.attachments;
+            // console.log('attachments', attachments.keys);
+
+            // const attachmentData = attachments.get(attachments.keys[0]);
+            cellProv = { ...cellProv, attachments: attachments.toJSON(), outputHTML } as MarkdownCellProvenance;
+          } else if (child instanceof RawCell) {
+            //RawCell extends cell
+            // no special information
+            // console.log('raw');
+          } else {
+            // console.log('unknown cell');
+          }
+          prov.cells.push(cellProv);
+        }
+      }
+
       this.nbtrrack.apply('execute', prov);
     } else {
-      console.warn(
-        'A different notebook was executed. Current notebook:',
+      // listener is still active while the notebook is open (removed when notebook closed)
+      console.debug(
+        'Waiting for executions of this notebook:',
+        this.notebook.title.label,
         this.notebook.id,
-        this.notebook.title,
-        'executed notebook:',
-        notebook.id,
-        notebook.title
+        '. Executed notebook:',
+        notebook.title.label,
+        notebook.id
       );
     }
   }
